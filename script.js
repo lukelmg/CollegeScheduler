b457dfc27ca85a673e70d0563e7d5c8de0ce94e8
lukelmg <lukegutman14@gmail.com>
2021-07-14 09:41:16 -0400

    dynamic sterff

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let blockHolder = $('block-holder');
+let blockHolder = document.getElementById('block-holder')
 
 let daysArray = ['M', 'T', 'W', 'R', 'F'];
+let dayNamesArray = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
 
 let classes = [];
 
@@ -14,10 +15,23 @@ classes[0] = {
 }
 
 for (let i = 0; i < daysArray.length; i++) {
+    let dayDiv = document.createElement('div');
+    dayDiv.id = daysArray[i];
+    dayDiv.className = 'day-div';
+    let dayHeader = document.createElement('h1');
+    dayHeader.innerHTML = dayNamesArray[i];
+    dayDiv.appendChild(dayHeader);
     for (let e = 0; e < classes.length; e++) {
         let currentDay = daysArray[i].toString();
         if ((classes[e].days).includes(currentDay)) {
-            console.log(classes[e])
+            let classDiv = document.createElement('div');
+            classDiv.id = classes[e].name;
+
+            let header = document.createElement('h2');
+            header.innerHTML = classes[e].name;
+            classDiv.appendChild(header);
+            dayDiv.appendChild(classDiv);
         }
     }
+    blockHolder.appendChild(dayDiv);
 }
