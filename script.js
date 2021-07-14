de0727868cafc06f04a02bcc1898cdb4cbfa3c4e
lukelmg <lukegutman14@gmail.com>
2021-07-13 22:28:46 -0400

    8 am's suck

---
+++ script.js
@@ -0,0 +1,12 @@
+let blockHolder = $('block-holder');
+
+let classes = [];
+
+classes[0] = {
+    name: 'Math',
+    start: '9:00',
+    end: '10:00',
+    days: 'MWF',
+    building: 'Smith',
+    room: '111'
+}
